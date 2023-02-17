79185e4faae21ca5f119706a5e29ab359ac208a1
Ha1dex <haidexCSGO@gmail.com>
2023-02-18 01:42:08 +0300

    life cycle 6

--- app/src/main/java/com/example/skyenglish/MainActivity.java
+++ app/src/main/java/com/example/skyenglish/MainActivity.java
@@ -44,6 +44,39 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Detail");
     }
     @Override
+    protected void onResume()
+    {
+        super .onResume();
+        setContentView(R.layout.activity_main);
+        Context info = getApplicationContext();
+        CharSequence message = "Resume activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+        Log.e(TAG,"Fail");
+        Log.w(TAG, "Warning");
+        Log.i(TAG, "Information");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Detail");
+    }
+    @Override
+    protected void onPause()
+    {
+        super .onPause();
+        setContentView(R.layout.activity_main);
+        Context info = getApplicationContext();
+        CharSequence message = "Pause activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+        Log.e(TAG,"Fail");
+        Log.w(TAG, "Warning");
+        Log.i(TAG, "Information");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Detail");
+    }
+
+    @Override
     protected void onStop()
     {
         super .onStop();
