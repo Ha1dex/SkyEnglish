addfb703299490c5294fb6f06a50beaf6dbef047
Ha1dex <haidexCSGO@gmail.com>
2023-02-18 00:04:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/skyenglish/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.skyenglish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
