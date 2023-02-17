9c4e405d219a1b2a49e4e3a9029cd1d2f8868dfd
Ha1dex <haidexCSGO@gmail.com>
2023-02-18 00:06:07 +0300

    life cycle 1

--- app/src/main/java/com/example/skyenglish/MainActivity.java
+++ app/src/main/java/com/example/skyenglish/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.skyenglish;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context info = getApplicationContext();
+        CharSequence message = "Created";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+        Log.e(TAG, "Fail");
+        Log.w(TAG, "Warning");
+        Log.i(TAG, "Information");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Detail");
+
     }
 }
